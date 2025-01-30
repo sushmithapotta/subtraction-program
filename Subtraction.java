91a3607474539eab11aad41aa6e06c8ec79b8f4b
sushmithapotta <pottasushmitha9@gmail.com>
2025-01-30 14:02:40 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,7 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int a=9;
+        int b=5;
+        System.out.printf("difference of %d - %d : %d",a,b,a-b);
+    }
+}
